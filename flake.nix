8fd5c5947ebad2e8e14c7770a884ab9f56e209f3
Yipeng Sun <syp@umd.edu>
2022-01-09 19:13:19 +0100

    Add gitinfo pre-commit hook in nix.

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       {
         devShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
           buildInputs = [
-            pkgs.pandoc
+            pkgs.gitinfo-hook
             (pkgs.texlive.combine {
               inherit (pkgs.texlive)
                 scheme-basic
@@ -31,18 +31,6 @@
                 ;
             })
           ];
-
-          FONTCONFIG_FILE = pkgs.makeFontsConf {
-            fontDirectories = with pkgs; [
-              nunito_sans
-            ];
-          };
-
-          shellHook = ''
-            # Make LaTeX aware of the custom beamer themes.
-            export TEXINPUTS=$(pwd)/themes:$(pwd)/pkgs//:
-            export PATH=$(pwd)/bin:$PATH
-          '';
         };
       });
 }
