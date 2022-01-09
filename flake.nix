8c068ffee98c11ce8af2b471418e66cf93fca369
Yipeng Sun <syp@umd.edu>
2022-01-09 19:17:19 +0100

    Add pre-commit-hooks, managed by nix.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
-  description = "Talks by me.";
+  description = "DCB design verification document.";
 
   inputs = {
     nixpkgs-pointer.url = "github:yipengsun/nixpkgs-pointer";
     nixpkgs.follows = "nixpkgs-pointer/nixpkgs";
     flake-utils.follows = "nixpkgs-pointer/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs-pointer, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs-pointer, nixpkgs, flake-utils, pre-commit-hooks }:
     {
       overlay = import ./nix/overlay.nix;
     } //
@@ -20,6 +21,7 @@
       in
       {
         devShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
+          name = "dcb_design_verfication_doc";
           buildInputs = [
             pkgs.gitinfo-hook
             (pkgs.texlive.combine {
